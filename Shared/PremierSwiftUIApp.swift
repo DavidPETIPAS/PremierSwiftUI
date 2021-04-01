368d5d4e41c25bb9c6cf396f60b11428b3003e54
David PETIPAS <david.petipas@gmail.com>
2021-04-01 07:59:01 +0200

    Premier commit

---
+++ Shared/PremierSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PremierSwiftUIApp.swift
+//  Shared
+//
+//  Created by David PETIPAS on 31/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct PremierSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
